a094c4f5af42919f2be448e8b16e782ca231c0ca
Manidhar Mulagapaka <manidhar.mulagapaka@gdn-commerce.com>
2019-07-10 21:20:01 +0530

    Basic

--- src/main/java/pages/CrashlyticsPage.java
+++ src/main/java/pages/CrashlyticsPage.java
@@ -1,4 +1,30 @@
 package pages;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static pages.FabricHomePage.driver;
 
 public class CrashlyticsPage {
+    public static void navigateToCrashlytics() throws InterruptedException {
+            Thread.sleep(3000);
+            driver().findElement(By.xpath("(//*[@class='flex-box align-items-center'])[4]")).click();
+            Thread.sleep(3000);
+    }
+    public static void buildNumber() throws InterruptedException {
+        Thread.sleep(5000);
+        driver().findElement(By.xpath("//*[@class='Select-arrow']")).click();
+        driver().findElement(By.xpath("//*[@class='Select-value']/span")).click();
+        Thread.sleep(7000);
+        driver().findElement(By.xpath("//*[@class='Select-arrow']")).click();
+        Thread.sleep(8000);
+        driver().findElement(By.xpath("(//*[@role='combobox'])[1]")).click();
+        driver().findElement(By.xpath("(//*[@role='combobox'])[1]")).sendKeys(Keys.ENTER);
+        Thread.sleep(7000);
+    }
+    public static void main(String[] args) throws InterruptedException {
+        FabricHomePage f =new FabricHomePage();
+        f.sample();
+        navigateToCrashlytics();
+
+    }
 }
--- src/main/java/pages/FabricHomePage.java
+++ src/main/java/pages/FabricHomePage.java
@@ -1,4 +1,48 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.serenitybdd.core.pages.PageObject;
+
 public class FabricHomePage {
+    @FindBy(xpath = "//*[@id='email']")
+    WebElementFacade enterUserName;
+
+    public static boolean isexecuted = false;
+    public static WebDriver driver;
+    public static void settingProperty(){
+        if(!isexecuted) {
+            System.setProperty("webdriver.chrome.driver", "/Users/Manidhar/Documents/craslytics-automation/crashlytics-logger/chromedriver");
+            driver = new ChromeDriver();
+            FabricHomePage.isexecuted = true;
+        }
+    }
+    public static WebDriver driver(){
+        FabricHomePage.settingProperty();
+        return driver;
+    }
+    public static void LaunchBrowser(){
+        driver().navigate().to("https://fabric.io/login/");
+    }
+    public void login(String username,String password){
+        enterUserName.sendKeys(username);
+        driver().findElement(By.xpath("//*[@id='password']")).sendKeys(password);
+        driver().findElement(By.xpath("//*[@class='block sdk-button sign-in']")).click();
+    }
+
+    public static void navigateToCrashlytics(String platformName) throws InterruptedException {
+        driver().findElement(By.xpath("//input[@class='inverse']")).sendKeys(platformName);
+        Thread.sleep(2000);
+        driver().findElement(By.xpath("//*[@class='flex-1 flex-box flex-column js-app-view']")).click();
+    }
+    public void sample() throws InterruptedException {
+        FabricHomePage.LaunchBrowser();
+        Thread.sleep(5000);
+        login("mradu.mishra@gdn-commerce.com", "Mradu123");
+        Thread.sleep(2000);
+        FabricHomePage.navigateToCrashlytics("Blibli Android");
+    }
 }
